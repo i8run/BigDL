accf7e3575a670575f49ba5dab2c4a16628bbd35
Wang Yanzhang <i8run15@gmail.com>
2017-04-24 09:40:11 +0800

    feat: add multi-thread supports

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/utils/perf/GEMM.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/utils/perf/GEMM.scala
@@ -17,9 +17,10 @@
 package com.intel.analytics.bigdl.utils.perf
 
 import com.intel.analytics.bigdl.tensor.DenseTensorBLAS
+import com.intel.analytics.bigdl.utils.ThreadPool
 
 object GEMM {
-  val rand = new scala.util.Random(5)
+  val iterations = 10
 
   def time[R](block: => R): (Double, R) = {
     val start = System.nanoTime()
@@ -29,48 +30,55 @@ object GEMM {
     ((end - start) / 1e6, result)
   }
 
-  def fillMatrix(m: Int, n: Int, matrix: Array[Float]): Unit = {
-    for (i <- 0 until m * n) {
+  def fillMatrix(length: Int, matrix: Array[Float]): Unit = {
+    val rand = new scala.util.Random(5)
+
+    for (i <- 0 until length) {
       matrix(i) = rand.nextFloat()
     }
   }
 
-  def gemm(transA: Char, transB: Char, m: Int, n: Int, k: Int): Unit = {
-    val matrixA = new Array[Float](m * k)
-    val matrixB = new Array[Float](k * n)
-    val matrixC = new Array[Float](m * n)
-
-    val iterations = 10
+  def gemm(transA: Char, transB: Char, m: Int, n: Int, k: Int, batchSize: Int): Double = {
+    val matrixA = new Array[Float](m * k * batchSize)
+    val matrixB = new Array[Float](k * n * batchSize)
+    val matrixC = new Array[Float](m * n * batchSize)
 
     val alpha = 1.0f
     val beta = 0.0f
 
     // load data to cache
-    fillMatrix(m, k, matrixA)
-    fillMatrix(k, n, matrixB)
-    fillMatrix(m, n, matrixC)
-    time[Unit] {
-      DenseTensorBLAS.gemm[Float]('N', 'N', m, n, k, alpha, matrixA, 0, m, matrixB, 0, n, beta,
-        matrixC, 0, m)
-    }._1
-
+    fillMatrix(m * k * batchSize, matrixA)
+    fillMatrix(k * n * batchSize, matrixB)
+    fillMatrix(m * n * batchSize, matrixC)
+    for (i <- 0 until batchSize) {
+      time[Unit] {
+        DenseTensorBLAS.gemm[Float](
+          'N', 'N', m, n, k,
+          alpha, matrixA, i * m * k, m,
+          matrixB, i * k * n, k, beta,
+          matrixC, i * m * n, m)
+      }._1
+    }
     // computing time
     var elapsed = 0.0
     for (i <- 0 until iterations) {
-      fillMatrix(m, k, matrixA)
-      fillMatrix(k, n, matrixB)
-      fillMatrix(m, n, matrixC)
+      fillMatrix(m * k * batchSize, matrixA)
+      fillMatrix(k * n * batchSize, matrixB)
+      fillMatrix(m * n * batchSize, matrixC)
 
-      elapsed += time[Unit] {
-        DenseTensorBLAS.gemm[Float](
-          transA, transB, m, n, k,
-          alpha, matrixA, 0, m,
-          matrixB, 0, n, beta,
-          matrixC, 0, m)
-      }._1
+      for (j <- 0 until batchSize) {
+        elapsed += time[Unit] {
+          DenseTensorBLAS.gemm[Float](
+            'N', 'N', m, n, k,
+            alpha, matrixA, j * m * k, m,
+            matrixB, j * k * n, k, beta,
+            matrixC, j * m * n, m)
+        }._1
+      }
     }
 
-    println(s"$transA, $transB -- Elapsed time: ${elapsed / iterations} ms")
+//    println(s"$m, $n, $k, ${elapsed / iterations} ms")
+    elapsed / iterations
 /*
     time[Unit] {
       DenseTensorBLAS.gemm[Float]('T', 'N', m, n, k, alpha, matrixA, 0, K, matrixB, 0, K, beta,
@@ -93,11 +101,38 @@ object GEMM {
   }
 
   def main(args: Array[String]) {
-    val m = args(1).toInt
-    val n = args(2).toInt
-    val k = args(3).toInt
+//    println(args.mkString("\t"))
+    val m = args(0).toInt
+    val n = args(1).toInt
+    val k = args(2).toInt
+    val batchSize = if (args.length >= 4) {
+      args(3).toInt // batchsize = 4
+    } else {
+      1 // batchsize = 1
+    }
 
-    gemm('N', 'N', m, n, k)
+    if (args.length > 5 && args(4).startsWith("singleThread")) {
+      val elapsed = gemm('N', 'N', m, n, k, batchSize)
+      println(s"$m, $n, $k, ${elapsed} ms")
+    } else {
+      val processors = Runtime.getRuntime.availableProcessors() / 2
+      val threads = new ThreadPool(processors)
+      val elapsedArray = new Array[Double](processors)
+
+      val start = System.nanoTime()
+      threads.invokeAndWait {
+        (0 until processors).map {
+          i => () => {
+            elapsedArray(i) = gemm('N', 'N', m, n, k, batchSize)
+          }
+        }
+      }
+      val end = System.nanoTime()
+
+      elapsedArray.foldLeft(0.0)((x, y) => x + y)
+
+      println(s"$m, $n, $k, ${(end - start) / 1e6 / iterations} ms")
+    }
   }
 }
 
